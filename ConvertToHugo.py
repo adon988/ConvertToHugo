7140a573215f0cb36dbf0e6a85fe4e1fad1715c0
mail6543210 <mail6543210@yahoo.com.tw>
2018-12-30 03:04:43 +0800

    Reorder imports

--- ConvertToHugo.py
+++ ConvertToHugo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+import argparse
+from datetime import datetime
 import os
 import re
-import yaml
-from datetime import datetime
-import argparse
 
+import yaml
 __author__ = 'coderzh'
 
 
